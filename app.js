9c10abe136287cfecfae29a5614ada2735e0cba7
HenryRivera01 <hariverao@udistrital.edu.co>
2025-05-27 00:28:25 -0500

    style: add logic comments

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ about.addEventListener('click', (e)=>{
 
 
 function removeActiveClass(elements){
-     elements.forEach((el)=>el.classList.remove('active'));
+     elements.forEach((el)=>el.classList.remove('active'));//iterates all buttons and remove the active class
 }
 
 function setActiveTab(button){
-    button.classList.add('active');
+    button.classList.add('active'); // Add the active class only in the clicked button
 }
 
 function showArticle(id){
     articles.forEach((article)=>{
-        article.classList.toggle('active', article.id === id)
+        article.classList.toggle('active', article.id === id) //Iterates all articles, then if the clicked one has the same id add the active class, else remove it
     })
 }
