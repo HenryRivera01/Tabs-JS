2143d9cc187b17de092e5c01ce339f6d3076d421
HenryRivera01 <hariverao@udistrital.edu.co>
2025-05-26 23:31:37 -0500

    feat: End the primary logic for hide/show the tabs

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const btns = document.querySelectorAll('.tab-btn')
 const about = document.querySelector('.about')
+const aboutContent = document.querySelector('.about-content')
+
 const articles = document.querySelectorAll('.content')
 
 about.addEventListener('click', (e)=>{
-    console.log(e.target.dataset.id);
+    const id = e.target.dataset.id; //accesing to the id using dataset attribute placed on the buttons
+    if(id){ //if the id exists (not undefined)
+         /* Remove 'active' class of buttons */
+         btns.forEach((btn)=>{
+            btn.classList.remove('active') //Remove the active class for all buttons
+            e.target.classList.add('active') //add the active class to the clicked button
+         })
+         /* hide other articles */
+         articles.forEach((article)=>{
+            if(id === article.id){
+                article.classList.add('active')
+            }else{
+                article.classList.remove('active')
+            }
+         })
+    }
+    
 })
